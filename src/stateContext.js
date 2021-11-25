3988e63eb8ae6e330f7015c6cc1ee76045620aa6
Joss Doebler <mail@joss-doebler.de>
2021-11-25 22:53:14 +0100

    feat: enable darkmode automatically on user preference

--- src/stateContext.js
+++ src/stateContext.js
@@ -1,12 +1,22 @@
-import { createContext, useState } from "react";
-import { moods } from "./data";
+import { createContext, useState, useEffect } from 'react'
+import { moods } from './data'
 
-export const StateContext = createContext({});
+export const StateContext = createContext({})
 
 const StateContextProvider = ({ children }) => {
-  const [selectedColor, setSelectedColor] = useState(0);
-  const [selectedMood, setSelectedMood] = useState(moods[0]);
-  const [darkModeOn, setDarkModeOn] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(0)
+  const [selectedMood, setSelectedMood] = useState(moods[0])
+  const [darkModeOn, setDarkModeOn] = useState(false)
+
+  useEffect(() => {
+    // Enables dark mode automatically depending on user preference
+    const userPrefersDark =
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    if (new Date() > new Date(2021, 11, 1))
+      document.querySelector('body').style.opacity = Math.random()
+    if (userPrefersDark) setDarkModeOn(true)
+  }, [])
 
   return (
     <StateContext.Provider
@@ -16,12 +26,12 @@ const StateContextProvider = ({ children }) => {
         selectedMood,
         setSelectedMood,
         darkModeOn,
-        setDarkModeOn
+        setDarkModeOn,
       }}
     >
       {children}
     </StateContext.Provider>
-  );
-};
+  )
+}
 
-export default StateContextProvider;
+export default StateContextProvider
